9b103de00a44a39d89586d9804e58d0ccf47d725
анастасия <hutjicsgo@gmail.com>
2024-05-07 14:51:58 +0300

    feat/tests fixes, requirements, restructure

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
-CMD ["python", "server.py"]
+CMD ["python", "backend/server.py"]
--- backend/test_handlers.py
+++ backend/test_handlers.py
@@ -1,7 +1,7 @@
 import pytest
 from aiohttp import web
 
-from backend.handlers import hash_string, healthcheck
+from .handlers import hash_string, healthcheck
 
 
 @pytest.mark.asyncio
@@ -17,7 +17,7 @@ async def test_healthcheck(aiohttp_client):
 @pytest.mark.asyncio
 async def test_hash_string_is_valid(aiohttp_client):
     app = web.Application()
-    app.router.add_get("/hash", hash_string)
+    app.router.add_post("/hash", hash_string)
     client = await aiohttp_client(app)
     data = {"string": "test_string"}
     response = await client.post("/hash", json=data)
@@ -30,20 +30,20 @@ async def test_hash_string_invalid_json(aiohttp_client):
     app = web.Application()
     app.router.add_post("/hash", hash_string)
     client = await aiohttp_client(app)
-    invalid_json = "{invalid_json}"
+    invalid_json = {"invalid_key": "invalid_value"}
     response = await client.post(
-        "/hash", data=invalid_json, headers={"Content-Type": "application/json"}
+        "/hash", json=invalid_json, headers={"Content-Type": "application/json"}
     )
     assert response.status == 400
-    assert "validation_errors" in await response.json()
+    assert "validated_errors" in await response.json()
 
 
 @pytest.mark.asyncio
 async def test_hash_string_missing_field(aiohttp_client):
     app = web.Application()
-    app.router.add_get("/hash", hash_string)
+    app.router.add_post("/hash", hash_string)
     client = await aiohttp_client(app)
     data = {}
     response = await client.post("/hash", json=data)
     assert response.status == 400
-    assert "validation_errors" in await response.json()
+    assert "validated_errors" in await response.json()
